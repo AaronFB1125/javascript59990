f557bade3c183bebddadc3f1e0b9bf087f654da5
Aaron Huapaya Angulo <ahuapaya.contacto@gmail.com>
2024-09-11 20:55:34 -0500

    Clase 01

---
+++ clase-1/js/index.js
@@ -0,0 +1,38 @@
+//*Comentario de saludo
+
+let saludo = "saludo";
+console.log(saludo)
+
+/* Generar comentarios
+en dos líneas*/
+
+//? Variable = Es un contenedor que almacena datos, los tipos de datos pueden ser varios
+//* numéricos, string, booleanos, arrays, objetos, funciones.
+
+let a = 0
+console.log(a)
+a = 1
+console.log(a)
+
+let VeinteYuno = "Veinte y uno"
+console.log(VeinteYuno)
+
+
+const VeintiDos = 22
+console.log(VeintiDos)
+
+
+let numero1 = 5
+let numero2 = 10
+let suma = numero1 + numero2
+
+console.log(`La suma del número ${numero1} y ${numero2} es: ${suma}`);
+
+
+let numerotres = parseInt(prompt("Por favor ingresa un número:"));
+let numerocuatro = parseInt(prompt("Ingresa otro número: "));
+
+// Convertir los valores ingresados a números antes de sumarlos
+let suma2 = numerotres + numerocuatro
+
+console.log(`El resultado de la suma es: ${suma2}`);
